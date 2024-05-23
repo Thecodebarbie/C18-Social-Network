be88054f24598f19d59914f5ad3892a2593aa898
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-22 22:39:00 -0400

    added email validation (required, unique, regex pattern matching)

--- models/User.js
+++ models/User.js
@@ -10,12 +10,16 @@ const userSchema = new Schema(
       trim:true
 
     },
-    last: String,
-    age: Number,
-    videos: [
+    email: {
+      type:String,
+      required:true,
+      unique:true,
+      match:[/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'video',
+        ref: 'Thought',
       },
     ],
   },
