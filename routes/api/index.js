2d5197a8ab82a8d369db161d0bbbed06816988f8
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-23 14:41:42 -0400

    saved

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-//http://localhost:3001/api/videos
+//http://localhost:3001/api/thoughts
 router.use('/thoughts', thoughtRoutes);
 
 //http://localhost:3001/api/users
