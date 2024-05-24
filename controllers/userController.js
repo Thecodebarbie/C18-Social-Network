0db76f73473b28fb2e4f67b5e4a01d2d64cd96bc
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-24 16:24:51 -0400

    Added updateUser function in userController.js for updating user information via PUT request.

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,4 +35,21 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async updateUser(req, res) {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'No user found with that ID' });
+      }
+
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,7 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser
 } = require('../../controllers/userController');
 
 // /api/users
@@ -10,6 +11,6 @@ const {
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateUser);
 
 module.exports = router;
